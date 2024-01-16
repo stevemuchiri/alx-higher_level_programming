ce9dbf1d6bdd5693346784f1025a1a1043cd4ac1
stevemuchiri <muchirindungu99@gmail.com>
2024-01-16 23:36:14 +0300

    This program will assign a random signed number to the variable number each time it is executed.

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
 import random
-
-
 number = random.randint(-100, 100)
-
-
 print("The number:", number)
-
 if number > 0:
     print("{}is positive".formart(number))
 elif number == 0:
